8d7b746beeeda345fab1f97fa18e46f60fb2eed9
AnnTumanina20_IT_17 <98600734+AnnTumanina@users.noreply.github.com>
2023-04-27 22:58:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+internal class Program
+    {
+        delegate string Day();
+        static void Main(string[] args)
+        {
+            string[] array = { "Пон", "Вт", "Ср", "Четв", "Пятн", "Суб", "Вос" };
+            int index = -1;
+
+            Day oneDay = () =>
+            {
+                index = (index + 1) % 7;
+                return array[index];
+            };
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(oneDay());
+            }
+
+            Console.ReadLine();
+        }
+    }
